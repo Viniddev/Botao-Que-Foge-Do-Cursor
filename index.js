4dab837348cbc0cec032efa8cf8752f0ba23865e
Vinicius Dias <diasvinicius95@outlook.com>
2022-09-23 18:15:10 -0300

    sumir texto

--- index.js
+++ index.js
@@ -40,4 +40,11 @@ sim.addEventListener("click", clicar)
 
 function clicar(){
     resp.innerHTML ="Eu te amo meu amoooor!!"
+    sim.style.transform="translate(0px)"
+    nao.style.transform="translate(0px, 0px)"
+
+    setTimeout(function(){
+        resp.style.opacity = 0;
+      }, 3000 )
+    
 }
