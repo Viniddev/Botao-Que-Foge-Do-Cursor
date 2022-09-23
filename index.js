5b9f10edcfc56ee7e9c5173892497c40bb788b75
Vinicius Dias <diasvinicius95@outlook.com>
2022-09-23 12:34:04 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,31 @@
+const sim = window.document.getElementById("opcyes")
+const nao = window.document.getElementById("opcNo")
+const fundo = window.document.getElementById("area")
+const resp = window.document.getElementById("textinho")
+
+nao.addEventListener("mouseenter", entrar)
+let estado = -1
+
+function entrar(){
+    estado++
+    if (estado == 0){
+        sim.style.transform="translate(+95px)"
+        nao.style.transform="translate(130px, -150px)"
+    }
+    if (estado == 1){
+        nao.style.transform="translate(130px, +150px)"
+    }
+    if (estado == 2){
+        nao.style.transform="translate(-325px, +150px)"
+    }
+    if (estado == 3){
+        nao.style.transform="translate(-325px, -150px)"
+        estado = -1
+    }
+}
+
+sim.addEventListener("click", clicar)
+
+function clicar(){
+    resp.innerHTML ="Eu te amo meu amoooor!!"
+}
